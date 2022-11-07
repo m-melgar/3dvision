c7c715fb2ecb1260ba7c5fc457b124b7f21d763d
cisu <marcos.melgar.segovia@gmail.com>
2022-11-07 15:13:58 +0100

    Function description.

--- utils.py
+++ utils.py
@@ -3,25 +3,25 @@ import scipy as scipy
 from scipy import linalg as LA
 
 
-def factorize_camera_matrix(P: np.ndarray) -> tuple(np.ndarray, np.ndarray, np.ndarray):
+def factorize_camera_matrix(p: np.ndarray) -> tuple(np.ndarray, np.ndarray, np.ndarray):
     """
-    Factorizes camera parameter P as K internal parameters, R external parameters and C camera center
-    :param P: np.ndarray([n.m])
+    Factorizes camera parameter P as K internal parameters, R external parameters and c camera center
+    :param p: np.ndarray([n.m])
             camera matrix P
     :return: np.ndarray([n.m]), np.ndarray([n.m]), np.ndarray([n.m])
-            K, R, C
+            K, R, c
     """
-    M = P[:, 0:3]
+    m = p[:, 0:3]
 
-    K, R = LA.rq(M)
+    k, r = LA.rq(m)
 
-    T = np.diag(np.sign(np.diag(K)))
+    t = np.diag(np.sign(np.diag(k)))
 
-    if scipy.linalg.det(T) < 0:
-        T[1, 1] *= -1
+    if scipy.linalg.det(t) < 0:
+        t[1, 1] *= -1
 
-    K = np.dot(K, T)
-    R = np.dot(T, R)
+    k = np.dot(k, t)
+    r = np.dot(t, r)
 
-    C = np.dot(LA.inv(-M), P[:, 3])
-    return K, R, C
+    c = np.dot(LA.inv(-m), p[:, 3])
+    return k, r, c
